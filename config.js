7b90f0e942ff0e0ad50a82a882cc4e92ea932ea6
Duncan Awerbuck <duk444@gmail.com>
2016-09-13 11:31:21 +0000

    Basic bot up & running!

--- config.js
+++ config.js
@@ -1,19 +1,20 @@
 module.exports = {
 
   // Port server listens on
-  "port"   : "8087",
+  "port"   : "8080",
 
   // The slash command shown in help text, e.g., /stuart
   "slash_command" : "stuart",
 
   // The Bot Persona
   "name"   : "Stuart",
-  "emoji"  : ":construction_worker:",
+  "emoji"  : ":robot_face:",
 
+  "host": "http://slack-stuart.githubuser919.c9users.io",
   // The Slack host
   //
   // Careful! Don't include the token with your slack_host value
-  "slack_host"  : "https://hooks.slack.com/services/FIXME/FIXME/FIXME",
-  "slack_token" : "FIXME"
+  "slack_host"  : "https://hooks.slack.com/services/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",
+  "slack_token" : "XXX"
 
 };
